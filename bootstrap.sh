2082a9e365379b1766eb886898d8fe81dabdb3bd
Michael Rolli <michael@rollis.ch>
2022-12-19 15:34:17 +0100

    Add shell configuration upon installing ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,23 +3,54 @@
 # All done with the system python3
 pip3_os="/usr/bin/python3 -m pip"
 
-# Install xcode command line tools
-xcode-select -p &>/dev/null || \
-  (echo "Installing Command Line Tools" && xcode-select --install)
+echo "Checking Xcode CLI tools"
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    tail -n 1 | sed 's/^[^C]* //')
+  softwareupdate -i "$PROD" --verbose;
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+else
+  echo "Xcode CLI tools OK"
+fi
+
+echo ""
 
 # upgrade pip to latest version
 echo "Upgrading pip"
-/usr/bin/python3 -m pip install --user --upgrade pip
+$pip3_os install --user --upgrade pip
 
 pkg_list=$($pip3_os list)
 # Install ansible related packages if needed
 echo "Now installing required packages"
-for pkg in ansible ansible-lint molecule paramiko yamllint
-do
+for pkg in ansible ansible-lint molecule paramiko yamllint; do
   [[ "$pkg_list" =~ ${pkg}[[:space:]] ]] || \
     ( echo "Installing ${pkg}" && $pip3_os install --user $pkg)
 done
 
+if [ "$SHELL" = /bin/zsh ]; then
+  shell_config=~/.zshrc
+else
+  shell_config=~/.bashrc
+fi
+
+if [ ! -f "$shell_config" ]; then
+  touch "$shell_config"
+fi
+
+if ! grep "Library/Python" ~/.zshrc &>/dev/null; then
+  echo "export PATH=~/Library/Python/3.9/bin/:$PATH" >> "$shell_config"
+fi
+
 echo "Installed all required packpages"
+
+if ! command -v ansible &>/dev/null; then
+  echo "Now run: source $shell_config"
+fi
 
 exit 0
